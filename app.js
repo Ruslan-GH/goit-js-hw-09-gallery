20146efb0706fd99c2a48df5d2b739ae103a714b
Ruslan-GH <82412111+Ruslan-GH@users.noreply.github.com>
2021-09-21 14:25:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,131 @@
+const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const gallery = document.querySelector('ul.js-gallery')
+const modalWindow = document.querySelector('.lightbox')
+const closeBtn = document.querySelector('button')
+const closeBackdrop = document.querySelector('div.lightbox__overlay')
+
+gallery.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems))
+
+gallery.addEventListener('click', openModalWindow)
+closeBtn.addEventListener('click', closeModalWindowBtn)
+closeBackdrop.addEventListener('click', closeModalWindowBackdrop)
+
+function createGalleryMarkup(gallery) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href=""${original}""
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`})
+  join('');
+  return 
+}
+
+const imageModalWindow = document.querySelector('img.lightbox__image')
+
+function openModalWindow(evt) {
+  window.addEventListener('keydown', closeModalWindowEsc)
+  evt.preventDefault()
+  modalWindow.classList.add('is-open');
+
+  imageModalWindow.src = evt.target.dataset.source;
+}
+
+function closeModalWindowBtn(evt) {
+  window.removeEventListener('keydown', closeModalWindowEsc)
+  modalWindow.classList.remove('is-open');
+
+  imageModalWindow.src = '';
+}
+
+function closeModalWindowBackdrop(evt) {
+  if (evt.currentTarget === evt.target) {
+    closeModalWindowBtn()
+  }
+}
+ 
+function closeModalWindowEsc(evt) {
+  if (evt.code === 'Escape') {
+    closeModalWindowBtn()
+  }
+}
+
+// function changeImageRight(evt) {
+//   if (evt.code === 'ArrowLeft') {
+//     console.log(imageModalWindow.src = elem.nextSibling.dataset.source);
+//   }
+//  }
+// // ArrowLeft ArrowRight
